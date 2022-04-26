be70357f7f792a2b932022e39169894f2aa5134d
foghill <ben@thefoghill.com>
2022-04-26 18:51:13 -0400

    commit tree 0c260a3bc7278d3d93b483cfc20645f34f36514d

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { image } from "../data/user";
 
-function About() {
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
       <p>I Made This</p>
-      <img src={image} alt="I made this" />
+      <img src={props.image} alt="I made this" />
     </div>
   );
 }
+
+export default About;
